a7221a8007f1dd3452f2d48330a387051ad49c67
Sanjar <sanjarubuntu@gmail.com>
2025-06-07 22:26:58 +0500

    Formatting login and register page

--- DjangoReactAuth/frontend/src/components/Register.jsx
+++ DjangoReactAuth/frontend/src/components/Register.jsx
@@ -1,5 +1,32 @@
+import "../App.css";
+import { Box } from "@mui/material";
+import MyTextField from "./forms/MyTextField";
+import MyPassField from "./forms/MyPassField";
+import MyButton from "./forms/MyButton";
+import { Link } from "react-router-dom";
+
 const Register = () => {
-  return <div>This is the Register page</div>;
+  return (
+    <div className={"myBackground"}>
+      <Box className={"whiteBox"}>
+        <Box className={"itemBox"}>
+          <Box className={"title"}>User Registration</Box>{" "}
+        </Box>
+        <Box className={"itemBox"}>
+          <MyTextField label={"Email"} />
+        </Box>
+        <Box className={"itemBox"}>
+          <MyPassField label={"Confirm password"} />
+        </Box>
+        <Box className={"itemBox"}>
+          <MyButton label={"Register"} />
+        </Box>
+        <Box className={"itemBox"}>
+          <Link to="/">Already Registered? Please Login</Link>
+        </Box>
+      </Box>
+    </div>
+  );
 };
 
 export default Register;
